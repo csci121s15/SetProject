7e899d7abc2468db78d35fd1053f44290a2ca1ac
Dan Howard <danhoward1994@gmail.com>
2015-03-30 14:49:18 -0400

    Updated

--- src/CardTest.java
+++ src/CardTest.java
@@ -40,27 +40,7 @@ public class CardTest extends TestCase{
     assertEquals("3RTS", card3.toString());
   }
   
-  public void testCardEquals()
-  {
-   Card card1 = new Card(1, 2, 3, 1);
-   Card card2 = new Card(1, 2, 3, 1);
-   Card card3 = new Card(4, 5, 6, 7);
-   Card card4 = new Card(-2, -1, 0, -5);
-   Card card5 = new Card(1, 1, 1, 1);
-   
-   assertEquals(true, card1.equals(card2));
-   assertEquals(true, card1.equals(card3));
-   assertEquals(true, card1.equals(card4));
-   assertEquals(true, card2.equals(card3));
-   assertEquals(true, card2.equals(card4));
-   assertEquals(true, card3.equals(card4));
-   
-   assertEquals(false, card5.equals(card1));
-   assertEquals(false, card5.equals(card2));
-   assertEquals(false, card5.equals(card3));
-   assertEquals(false, card5.equals(card4));
-   
-  }
 }
+
 
   
