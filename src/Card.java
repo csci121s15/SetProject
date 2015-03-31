41913f198281c3a8c37279f0c109173332baa718
Hailey Kester <sthmk01@moravian.edu>
2015-03-31 17:54:46 -0400

    Card part of Set Project Complete!

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,98 @@
 public class Card {
-  // Create the rest of this class yourself
+  private int quantity;
+  private int color;
+  private int shading;
+  private int shape; 
+  private int value;
   
+  public Card(int theQuantity, int theColor, int theShading, int theShape) {
+    quantity = theQuantity;
+    quantity = ((((theQuantity % 3) + 3) % 3));
+    
+    color = theColor;
+    color = ((((theColor % 3) + 3) % 3));
+    
+    shading = theShading;
+    shading = ((((theShading % 3) + 3) % 3));
+    
+    shape = theShape;
+    shape = ((((theShape % 3) + 3) % 3));
+    
+  }
+  //Got the valueToFix from class//
+  private int the(int valueToFix) {
+    if (valueToFix < 1 || valueToFix > 3)
+      return (((valueToFix % 3) + 3) % 3) + 1;
+    else
+      return valueToFix;
+  }
+  
+  //getValue() goes with valueToFix above//
+  public int getValue() {
+    return value;
+  }
+ 
+  
+  
+  public int getQuantity()  {
+    return quantity;
+  }
+  
+  public int getColor() {
+    return color;
+  }
+  
+  public int getShading() {
+    return shading;
+  }
+  
+  public int getShape() {
+    return shape;
+  }
+  
+  public boolean isSet(Card card1, Card card2) {
+    boolean isSet = true;
+    return (quantity + card1.getQuantity() + card2.getQuantity()) % 3 == 0 &&
+      (color + card1.getColor() + card2.getColor()) % 3 == 0 &&
+      (shading + card1.getShading() + card2.getShading()) % 3 == 0 &&
+      (shape + card1.getShape() + card2.getShape()) % 3 == 0;
+   
+    
+  }
+  
+  public String toString() {
+    String string = "" + quantity;
+    String colorString = "";
+    String shadingString = "";
+    String shapeString = "";
+    
+    
+    if (color == 1)
+      colorString = "R";
+    if (color == 2) 
+      colorString = "G";
+    if (color == 3)
+      colorString = "P";
+    
+    if (shading == 1)
+      shadingString = "O";
+    if (shading == 2)
+      shadingString = "T";
+    if (shading == 3)
+      shadingString = "S";
+    
+    if (shape == 1)
+      shapeString = "O";
+    if (shape == 2) 
+      shapeString = "D";
+    if (shape == 3)
+      shapeString = "S";
+    
+    return string = quantity + colorString + shadingString + shapeString;
+   
+  }
+
+
   public boolean equals(Object obj) {
     Card that = (Card)obj;
     
---
+++ src/CardTest.java
@@ -0,0 +1,77 @@
+import junit.framework.TestCase;
+
+/**
+ * A JUnit test case class.
+ * Every method starting with the word "test" will be called when running
+ * the test with JUnit.
+ */
+public class CardTest extends TestCase {
+  
+  /**
+   * A test method.
+   * (Replace "X" with a name describing the test.  You may write as
+   * many "testSomething" methods in this class as you wish, and each
+   * one will be called when running JUnit over this class.)
+   */
+  
+  public void testTheSetValues() {
+    Card card1 = new Card(1, 1, 1, 1);
+    assertEquals(1, card1.getQuantity());
+    assertEquals(1, card1.getColor());
+    assertEquals(1, card1.getShading());
+    assertEquals(1, card1.getShape());
+    assertEquals("1ROO", card1.toString());
+  }
+  
+  public void testNegativeSetValues() {
+    Card card1 = new Card(-4, -4, -4, -4);
+    assertEquals("2GTD", card1.toString());
+  }
+  
+  public void testSetIsLegal() {
+    Card card1 = new Card(2, 2, 1, 1);
+    Card card2 = new Card(2, 2, 2, 2);
+    Card card3 = new Card(2, 2, 3, 3);
+    assertEquals(true, card1.isSet(card2, card3));
+                   
+  }
+  
+  public void testSetIsNotLegal() {
+    Card card1 = new Card(1, 1, 2, 2);
+    Card card2 = new Card(2, 1, 2, 2);
+    Card card3 = new Card(2, 2, 2, 2);
+    assertEquals(false, card2.isSet(card1, card3));
+  }
+  
+  public void testAllCardsSame() {
+    Card card1 = new Card(3, 2, 1, 3);
+    Card card2 = new Card(3, 2, 1, 3);
+    Card card3 = new Card(3, 2, 1, 3);
+    assertEquals(true, card3.isSet(card1, card2));
+  }
+  
+  public void testSameColor() {
+    Card card1 = new Card(1, 1, 2, 3);
+    Card card2 = new Card(2, 1, 1, 2);
+    Card card3 = new Card(3, 1, 3, 1);
+    assertEquals(true, card1.isSet(card2, card3));
+  }
+  
+  public void testAllRandomCards() {
+    Card card1 = new Card(1, 2, 3, 1);
+    Card card2 = new Card(2, 3, 1, 2);
+    Card card3 = new Card(3, 1, 2, 3);
+    assertEquals(true, card2.isSet(card1, card3));
+  }
+  
+  public void testSameShade() {
+    Card card1 = new Card(1, 3, 3, 2);
+    Card card2 = new Card(2, 2, 3, 2);
+    Card card3 = new Card(2, 3, 3, 2);
+    assertEquals(false, card3.isSet(card2, card1));
+  }
+                 
+                
+                               
+
+}
