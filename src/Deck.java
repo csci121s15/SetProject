5051e4a7a9d102d8dabcaef7c5ebe5e36ca5057c
Marisa Pacitti <stmtp05@moravian.edu>
2015-04-08 08:55:25 -0400

    Deck and Deck Test

--- src/Deck.java
+++ src/Deck.java
@@ -2,8 +2,47 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Deck {
+  public ArrayList<Card> cards; 
+  public int nextCardIndex;
+  
+  public Deck() {
+    cards = new ArrayList<Card>(81);
+    for(int var1 = 1; var1 <= 3; var1++) {
+      for(int var2 = 1; var2 <= 3; var2++) {
+        for(int var3 = 1; var3 <= 3; var3++) {
+          for(int var4 = 1; var4 <= 3; var4++) {
+             cards.add(new Card(var1, var2, var3, var4));
+          }
+        }
+      }
+    }
+    int nextCardIndex = 0;
+    Collections.shuffle(cards);
+  }
+  
+  public boolean hasNext() {
+    if (nextCardIndex >= cards.size()) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  
+  public Card getNext() {
+    if (hasNext() == false) {
+      return null;
+    }
+    else {
+      nextCardIndex += 1;
+      return cards.get(nextCardIndex - 1);
+    }
+  }
+  
   // Implement the rest of this class yourself
   
   public Deck(String filename) {
