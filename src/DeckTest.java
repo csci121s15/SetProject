9628080a5092a87d13feec9ec6d279b1a74e19e0
ChrisSheehan01 <stcts02@moravian.edu>
2015-04-08 00:03:29 -0400

    Deck and DeckTest commit

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -13,7 +13,24 @@ public class DeckTest extends TestCase {
    * many "testSomething" methods in this class as you wish, and each
    * one will be called when running JUnit over this class.)
    */
-  public void testX() {
+  public void testAllCards() {
+    Deck d = new Deck("81cards.txt");
+    for (int i=0; i<81; i++) {     
+      assertEquals(d.hasNext(), true);
+      d.getNext();
+    }
+    d.getNext();
+    assertEquals(d.hasNext(), false);
+  }
+  
+  public void test3Cards() {
+    Deck d = new Deck("3cards.txt");
+    for (int i=0; i<3; i++) {
+      assertEquals(d.hasNext(), true);
+      d.getNext();
+    }
+    d.getNext();
+    assertEquals(d.hasNext(), false);
   }
   
 }
