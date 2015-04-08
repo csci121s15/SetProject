a9960186ee22b77afd6f9d00d5591cece2054eb2
Carly Thorpe <stcat04@moravian.edu>
2015-04-08 00:10:31 -0400

    Deck class and test

--- src/Deck.java
+++ src/Deck.java
@@ -2,9 +2,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
-  // Implement the rest of this class yourself
+  private int nextCardIndex;
+  private ArrayList<Card> cards;
+  
+  public Deck() {
+    cards = new ArrayList<Card>(81);
+    
+    for (int Q = 1; Q <= 3; Q++) {
+      for (int C = 1; C <= 3; C++) {
+        for (int S = 1; S <= 3; S++) {
+          for (int Sh = 1; Sh <= 3; Sh++) {
+            Card c = new Card(Q, C, S, Sh);
+            cards.add(c);
+          }
+        }
+      }
+    }
+    Collections.shuffle(cards);
+  }
+  
+
   
   public Deck(String filename) {
     cards = new ArrayList<Card>(81);
@@ -40,6 +60,25 @@ public class Deck {
     }
     catch(Exception e) {
       throw new RuntimeException("Error while reading file: " + e.toString());
+    }
+  }
+  
+  public boolean hasNext() {
+    if (nextCardIndex < cards.size()) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  public Card getNext() {
+    if (hasNext() != false) {
+      nextCardIndex += 1;
+      return cards.get(nextCardIndex - 1);
+    }
+    else {
+      return null;
     }
   }
 }
