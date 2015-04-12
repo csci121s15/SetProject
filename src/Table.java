018448b67987beab19c8bb7c811c1971a46327b6
Chris Sheehan <stcts02@moravian.edu>
2015-04-11 20:30:43 -0400

    Table and Table Node

---
+++ src/Table.java
@@ -0,0 +1,32 @@
+public class Table{
+  private TableNode head;
+//  private int theLength;
+  
+
+  public Table()
+    //
+  {
+    head = null;
+//    theLength = 0;
+  }
+  
+  public void add(Card){
+    //
+  }
+  
+  public void removeSet(Card, Card, Card){
+    //removes set from table
+  }
+  
+  public int numCards(){
+    //
+  }
+  
+  public Card getCard(int index){
+    //returns card at given int
+  }
+  
+  public int numSets(){
+    //returns the number of Sets on the table
+  }
+}
---
+++ src/TableNode.java
@@ -0,0 +1,21 @@
+public class TableNode{
+  private Card card;
+  private TableNode next;
+  
+  public TableNode(Card){
+  }
+  
+  public void setNext(Node newNext){
+    next = newNext;
+  }
+    
+  public TableNode getNext(){
+    return next;
+  }
+  
+  public Card getCard(){
+    return Card;
+  }
+  
+}
+
