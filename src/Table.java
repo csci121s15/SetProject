e10dfcc18eeb2bfee870b4ebf5a95013cf11e95a
Marlon Moraga <stmfm05@moravian.edu>
2015-04-16 15:05:14 -0400

    Updated Table (New version)

---
+++ src/Table.java
@@ -0,0 +1,111 @@
+public class Table {
+  private TableNode head;
+  private int length;
+  
+  public Table() {
+    //initialize head
+    head = null;
+  }
+  
+  public void add(Card marlon) {
+    TableNode newNode = new TableNode(marlon);
+    newNode.setNext(head);
+    head = newNode;
+  }
+  
+  private boolean onTable(Card m){
+    TableNode curr = head;
+    
+    while(curr != null){
+      if(m.equals(curr.getCard())){
+        return true;
+      }
+      else{
+       curr = curr.getNext();
+      }
+    }
+    return false;
+  }
+  private void removeCard(Card m){
+    TableNode prev = null;
+    TableNode curr = head;
+    if (m.equals(head.getCard()))
+      head = head.getNext();  
+    else{
+      while(!m.equals(curr.getCard())){
+        prev = curr;
+        curr = curr.getNext();
+      }
+      prev.setNext(curr.getNext());
+    }
+  }
+
+      
+    
+      
+  
+  public void removeSet(Card m1, Card m2, Card m3) {
+    if(m1.isSet(m2, m3) == false)
+      return;
+    if(!onTable(m1))
+      return;
+    if(!onTable(m2))
+      return;
+    if(!onTable(m3))
+      return;
+    removeCard(m1);
+    removeCard(m2);
+    removeCard(m3);
+  }
+        
+  
+  public int numCards(){
+    TableNode temp = head;
+    if(head == null)
+      return 0;
+    while(temp != null)
+    {
+      length++;
+      temp = temp.getNext();
+    }
+    return length;
+  }
+  
+  public Card getCard(int index) {
+    TableNode curr = head;
+    if(head == null)
+      return null;
+    if(index < numCards())
+    {
+      for(int i = 0; i<numCards();i++)
+      {
+        curr.getNext();
+      }
+      return curr.getCard();
+    }
+    else
+      return null;
+  }
+  
+  public int numSets() {
+    TableNode m = head;
+    TableNode mm = m.getNext();
+    TableNode mmm = mm.getNext();
+    int numSets = 0;
+    
+    while(m != null && m.getNext() != null && m.getNext().getNext() != null){
+      mm = m.getNext();
+      while(mm != null && mm.getNext() != null){
+        mmm = mm.getNext();
+        while(mmm != null){
+          if(m.getCard().isSet(mm.getCard(),mmm.getCard()))
+            numSets++;
+          mmm = mmm.getNext();
+        }
+        mm = mm.getNext();
+      }
+      m = m.getNext();
+    }
+    return numSets;
+  }
+}
