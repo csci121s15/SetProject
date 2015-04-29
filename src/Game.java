5705d1de9292582f4903ae0759d2bd45772decd9
Carly Thorpe <stcat04@moravian.edu>
2015-04-28 22:01:05 -0400

    Fixed Game, Monte Carlo Simulation

--- src/Game.java
+++ src/Game.java
@@ -3,8 +3,8 @@ public class Game {
   private Deck d;
   
   public Game() {
-    Deck d = new Deck();
-    Table t = new Table();
+    d = new Deck();
+    t = new Table();
     
     for (int i = 0; i < 12; i++) {
       t.add(d.getNext());
@@ -12,11 +12,13 @@ public class Game {
   }
   
   public Game(String filename) {
-    Deck d = new Deck(filename);
-    Table t = new Table();
+    d = new Deck(filename);
+    t = new Table();
     
     for (int i = 0; i <= 12; i++) {
-      t.add(d.getNext());
+      if (d.hasNext() == true) {
+        t.add(d.getNext());
+      }
       
       if (t.numCards() == 12) {
         return;
@@ -24,41 +26,43 @@ public class Game {
     }
   }
   
-    public void playRound() {
+  public void playRound() {
+    boolean foundSet = false;
     if(t.numSets() == 0 && d.hasNext() != false) {
-        for (int i = 0; i < 3; i++) {
-          t.add(d.getNext());
-          return;
-        }
-        for (int i = 0; i < numCards() - 2; i++) {
-          for (int j = i + 1; i < numCards() - 1; j++) {
-            for (int k = j + 1; j < numCards(); k++) {
-              Card c1 = t.getCard(i);
-              Card c2 = t.getCard(j);
-              Card c3 = t.getCard(k);
-              
-              if (t.getCard(i).isSet(t.getCard(j), t.getCard(k))) {
-                t.removeSet(t.getCard(i), t.getCard(j), t.getCard(k));
-              }
-              if(t.numCards() < 9) {
-                for (int l = 0; l < 3; l++) {
-                  t.add(d.getNext());
-                }
-                return;
-              }
-            }
-          }
-        }
-    
-        if (t.numSets() == 0) {
-          for (int m = 0; m < 3; m++) {
-            t.add(d.getNext());
-          }
-        }
+      for (int i = 0; i < 3; i++) {
+        t.add(d.getNext());
+      }
+      return;
     }
-    }
+    for (int i = 0; i < numCards() - 2; i++) {
+      if (foundSet) 
+        break;
+      for (int j = i + 1; j < numCards() - 1; j++) {
+        if (foundSet)
+          break;
+        for (int k = j + 1; k < numCards(); k++) {
+          Card c1 = t.getCard(i);
+          Card c2 = t.getCard(j);
+          Card c3 = t.getCard(k);
           
-
+          if (c1.isSet(c2, c3)) {
+            t.removeSet(c1, c2, c3);
+            foundSet = true;
+            break;
+          }
+        }
+      }
+    }
+    if(t.numCards() < 12 && d.hasNext() == true) {
+      for (int l = 0; l < 3; l++) {
+        if (d.hasNext() == true) 
+          t.add(d.getNext());
+      }
+    }
+  }
+  
+  
+  
   
   public int numSets() {
     return t.numSets();
