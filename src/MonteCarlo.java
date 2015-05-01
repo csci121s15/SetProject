88f1faf78c3244fd56d478a55dd678b5fb661965
ChrisSheehan01 <stcts02@moravian.edu>
2015-04-30 22:00:40 -0400

    MonteCarlo

--- src/MonteCarlo.java
+++ src/MonteCarlo.java
@@ -22,34 +22,22 @@ public class MonteCarlo{
     //use a main class
     
     
-    //1st Question:
+    //1st Question && 2nd Question:
     double avgNumberOfSets = 0;
+    double avgNumberOfCards = 0;
     int i = 0;
-    while(i<=1000000){
+    while(i<=10000000){
       Game g = new Game();
       avgNumberOfSets += g.numSets();
+      while(g.isGameOver() == false) {
+        g.playRound();   
+      }
+      avgNumberOfCards += g.numCards();
       i++;   
     }
     System.out.println("The average Number of Sets in " + (i-1) + " sets of random cards is " + avgNumberOfSets/(i-1));
-    
-    
-    //2nd question:
-    double avgNumberOfCards = 0;
-    int j = 0;
-    while(j<=1000000){  /// this takes about 9 minutes..
-      Game g2 = new Game();
-      while(g2.isGameOver() == false) {
-        g2.playRound();   
-      }
-      avgNumberOfCards += g2.numCards();
-      j++;
-    }
-    System.out.println("The average Number of Cards left on the table at the end of a game after " + (j-1) + " games is " + avgNumberOfCards/(j-1));
-    
-    
+    System.out.println("The average Number of Cards left on the table at the end of a game after " + (i-1) + " games is " + avgNumberOfCards/(i-1));
   
   
-  
-  //end of class
   }
 }
