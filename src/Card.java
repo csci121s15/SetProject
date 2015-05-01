5aa426396c3935ec4533c41a62ea5a02441c3ba1
Marisa Pacitti <stmtp05@moravian.edu>
2015-05-01 09:21:09 -0400

    Monte Carlo Simulation and Updated Card and Game

--- src/Card.java
+++ src/Card.java
@@ -3,14 +3,26 @@ public class Card {
   public int color;
   public int shading;
   public int shape;
-  
+
   public Card (int cardQuantity, int cardColor, int cardShading, int cardShape){
-    quantity = ((((cardQuantity % 3)+3)%3)+1);
-    color = ((((cardColor % 3)+3)%3)+1);
-    shading = ((((cardShading % 3)+3)%3)+1);
-    shape = ((((cardShape % 3)+3)%3)+1);
+    quantity = formValue(cardQuantity);
+    color = formValue(cardColor);;
+    shading = formValue(cardShading);;
+    shape = formValue(cardShape);;
   }
   
+  private int formValue(int val) {
+    if (val < 4 && val > 0) {
+      return val;
+    }
+    else if (val >= 0) {
+      return val % 3 + 1;
+    }
+    else {
+      return (((val % 3) + 3) % 3) +1;
+    }
+  }
+
   public int getQuantity() {
     return quantity;
   }
@@ -28,49 +40,82 @@ public class Card {
   }
   
   public boolean isSet (Card cardA, Card cardB) {
-    if ((((quantity + cardA.getQuantity() + cardA.getQuantity())%3)==0) && 
-        (((color + cardA.getColor() + cardA.getColor())%3)==0) && 
-        (((shading + cardA.getShading() + cardA.getShading())%3)==0) && 
-        (((shape + cardA.getShape() + cardA.getShape())%3)==0))
+    if ((((quantity + cardA.getQuantity() + cardB.getQuantity())%3)==0) && 
+        (((color + cardA.getColor() + cardB.getColor())%3)==0) && 
+        (((shading + cardA.getShading() + cardB.getShading())%3)==0) && 
+        (((shape + cardA.getShape() + cardB.getShape())%3)==0)) {
       return true;
-  else 
+    }
+    else {
     return false;
+    }
+  }
+  
+  public boolean isRightForSet(int val1, int val2, int val3) {
+    if ((val1 + val2 + val3) % 3 == 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
   
   public String toString() {
     String str = "";
+    String quant = "";
     String col = "";
     String shade = "";
     String sha = "";
     
-    if (color == 1)
-      col = "R";
-    else if (color == 2)
-      col = "G";
-    else 
-      col = "P";
+    if (quantity == 1) {
+      str += "1";
+    }
+    else if (quantity == 2) {
+      str += "2";
+    }
+    else {
+      str += "3";
+    }
     
-    if (shading == 1)
-      shade = "O";
-    else if (color == 2)
-      shade = "T";
-    else 
-      shade = "S";
+    if (color == 1) {
+      str += "R";
+    }
+    else if (color == 2){
+      str += "G";
+    }
+    else {
+      str += "P";
+    }
     
-    if (shape == 1)
-      sha = "O";
-    else if (color == 2)
-      sha = "D";
-    else 
-      sha = "S";
+    if (shading == 1) {
+      str += "O";
+    }
+    else if (shading == 2) {
+      str += "T";
+    }
+    else {
+      str += "S";
+    }
     
-    return str = quantity + col + shade + sha;
+    if (shape == 1) {
+      str += "O";
+    }
+    else if (shape == 2) {
+      str += "D";
+    }
+    else {
+      str += "S";
+    }
+    
+    return str = quant+ col + shade + sha;
+  }
   
+  public boolean equals(Object obj) {
     Card that = (Card)obj;
-    
     return quantity == that.getQuantity() &&
       color == that.getColor() &&
       shading == that.getShading() &&
       shape == that.getShape();
   }
 }
+
--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,20 @@
 public class Game {
-  private Table t = new Table();
-  private Deck d = new Deck();
+  private Table t;
+  private Deck d;
   
   public Game() {
-    Deck d = new Deck();
+    d = new Deck();
+    t = new Table();
     for (int i = 0; i < 12; i++) {
       t.add(d.getNext());
     }  
   }
   
   public Game(String filename) {
-    Deck d = new Deck(filename);
-    for (int i=0; i < 12; i++) {
-      d.getNext();
+    d = new Deck(filename);
+    t = new Table();
+    for (int i=0; i < 12 && d.hasNext(); i++) {
+      t.add(d.getNext());
     }
   }
   
@@ -27,20 +29,26 @@ public class Game {
   public void playRound() {
     if (d.hasNext() == true && t.numSets() == 0) {
       for (int r = 0; r < 3; r++) {
-          t.add(d.getNext());
-        }
-        return;
+        if (d.hasNext() == false)
+          return;
+        t.add(d.getNext());
       }
+      return;
+    }
     
-    if (d.hasNext() == true && t.numSets() > 0) {
+    if (d.hasNext() == true && t.numSets() != 0) {
       for (int i = 0; i < t.numCards() - 2; i++) {
         for (int j = i + 1; j < t.numCards() - 1; j++) {
           for (int k = j + 1; k < t.numCards(); k++) {
-            if (t.getCard(i).isSet(t.getCard(j), t.getCard(k))) {
+            if (t.getCard(i).isSet(t.getCard(j), t.getCard(k)) == true) {
               t.removeSet(t.getCard(i), t.getCard(j), t.getCard(k));
-              for (int r = 0; r < 3; r++) {
+              for (int r = 0; t.numCards() < 12; r++) {
+                if (d.hasNext() == false) {
+                  return;
+                }
                 t.add(d.getNext());
-              }   
+              }
+              return;
             }
           }
         }
@@ -51,16 +59,17 @@ public class Game {
       for (int i = 0; i < t.numCards() - 2; i++) {
         for (int j = i + 1; j < t.numCards() - 1; j++) {
           for (int k = j + 1; k < t.numCards(); k++) {
-            if (t.getCard(i).isSet(t.getCard(j), t.getCard(k))) {
+            if (t.getCard(i).isSet(t.getCard(j), t.getCard(k)) == true) {
               t.removeSet(t.getCard(i), t.getCard(j), t.getCard(k));
-              return;
+                return;
             }
           }
         }
       }
     }
+  
     
-    if (d.hasNext() == false && t.numSets() > 0) {
+    if (d.hasNext() == false && t.numSets() != 0) {
       for (int i = 0; i < t.numCards() - 2; i++) {
         for (int j = i + 1; j < t.numCards() - 1; j++) {
           for (int k = j + 1; k < t.numCards(); k++) {
@@ -72,14 +81,11 @@ public class Game {
         }
       }
     }
-    
-    if (d.hasNext() == false && t.numSets() == 0) {
-      isGameOver();
-    }
+    return;
   }
-         
+          
   public boolean isGameOver() {
-    if (d.hasNext() == false && t.numSets() == 0) {
+    if (!d.hasNext() && t.numSets() == 0) {
       return true;
     }
     else {
